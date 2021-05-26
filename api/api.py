703d29445b8cd11c49cd32fccd8aa6f337d3984d
Michael Teolis <michaelteolis96@gmail.com>
2021-05-26 16:56:16 -0400

    Add sql alchemy and migrations for postgresql database

--- api/api.py
+++ api/api.py
@@ -1,10 +1,17 @@
 import time
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from env_secrets import env_secrets
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = env_secrets.sqlalchemy_database_uri
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time(), 'neep': 'neep'}
+    return {'time': time.time(), 'noot': 'n00t'}
 
