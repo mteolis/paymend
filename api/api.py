4374bcaf0dee4f4e339241eea7cccff17d9e2643
Michael Teolis <michaelteolis96@gmail.com>
2021-05-26 20:15:46 -0400

    Add sql alchemy and migrate to api

--- api/api.py
+++ api/api.py
@@ -1,7 +1,12 @@
 import time
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/paymend'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/time')
